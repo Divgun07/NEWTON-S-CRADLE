d66ea1be4f1c4107a46b4d45e81240886bc9279e
Divgun07 <71441340+Divgun07@users.noreply.github.com>
2020-10-22 17:07:51 +0530

    Update rope.js

--- rope.js
+++ rope.js
@@ -25,8 +25,8 @@ display()
     var Anchor1X=pointA.x;
     var Anchor1Y=pointA.y;
 
-    var Anchor2X=pointA.x+this.offsetX;
-    var Anchor2Y=pointA.y+this.offsetY;
+    var Anchor2X=pointB.x+this.offsetX;
+    var Anchor2Y=pointB.y+this.offsetY;
 
 line(Anchor1X, Anchor1Y,Anchor2X, Anchor2Y);
 }
